f240a02a10e87d920c4bc17eda968e3b3d253cb3
Sang-Heon Jeon <ekffu200098@gmail.com>
2020-08-03 22:33:55 +0900

    OBJECT 14장 코드 작성 예제 완성

--- OBJECTS/PhoneCharge/phone.py
+++ OBJECTS/PhoneCharge/phone.py
@@ -1,12 +1,16 @@
 from call import Call
 from typing import List
-from ratepolicy import RatePolicy
+from ratepolicy import AdditionalPolicy, BasePolicy
 
 
 class Phone():
-    def __init__(self, calls: List[Call], rate_policy: RatePolicy):
+    def __init__(self, calls: List[Call], base_policy: BasePolicy, additional_policies: List[AdditionalPolicy]) -> None:
         self.calls = calls
-        self.rate_policy = rate_policy
+        self.base_policy = base_policy
+        self.addtional_policies = additional_policies
 
     def calcuate_fee(self) -> int:
-        return self.rate_policy.calcuate_fee(self.calls)
+        fee = self.base_policy.calcuate_fee(self.calls)
+        for policy in self.addtional_policies:
+            fee = policy.apply_policy_to_fee(fee)
+        return fee
--- OBJECTS/PhoneCharge/ratepolicy.py
+++ OBJECTS/PhoneCharge/ratepolicy.py
@@ -3,12 +3,7 @@ from feepolicy import FeePolicy
 from typing import List
 
 
-class RatePolicy():
-    def calcuate_fee(self, calls: List[Call]) -> int:
-        raise NotImplementedError()
-
-
-class BasePolicy(RatePolicy):
+class BasePolicy():
     def __init__(self, fee_policies: List[FeePolicy]) -> int:
         self.fee_policies = fee_policies
 
@@ -17,3 +12,24 @@ class BasePolicy(RatePolicy):
         for fee_policy in self.fee_policies:
             fee += fee_policy.calculate_fee(calls)
         return fee
+
+
+class AddtionalPolicy():
+    def apply_policy_to_fee(self, fee: int) -> int:
+        NotImplementedError()
+
+
+class TaxPolicy(AddtionalPolicy):
+    def __init__(self, tax_ratio: float) -> None:
+        self.tax_ratio = tax_ratio
+
+    def apply_policy_to_fee(self, fee: int) -> int:
+        return fee * (1 + self.tax_ratio)
+
+
+class DiscountPolicy(AddtionalPolicy):
+    def __init__(self, discount_amount: int) -> None:
+        self.discount_amount = discount_amount
+
+    def apply_policy_to_fee(self, fee: int) -> int:
+        return fee - self.discount_amount
